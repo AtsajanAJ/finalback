a399354549fa159282e40cb0b37c90741142f39c
AtsajanAJ <atsajan08@gmail.com>
2023-12-01 01:54:41 +0700

    forrgot env

--- app/models/db.js
+++ app/models/db.js
@@ -2,16 +2,16 @@ const mysql = require('mysql2');
 const dbConfig = require('../config/db.config');
 
 //Connection with database models
-const connection = mysql.createConnection({
-    host: dbConfig.HOST,
-    user: dbConfig.USER,
-    password: dbConfig.PASSWORD,
-    database: dbConfig.DB
-});
+// const connection = mysql.createConnection({
+//     host: dbConfig.HOST,
+//     user: dbConfig.USER,
+//     password: dbConfig.PASSWORD,
+//     database: dbConfig.DB
+// });
 
-// db.js
-// require("dotenv").config()
-// const connection = mysql.createConnection(process.env.DATABASE_URL)
+db.js
+require("dotenv").config()
+const connection = mysql.createConnection(process.env.DATABASE_URL)
 
 connection.connect((error)=>{
     if(error) console.log("MySQL Connection: " +error);
